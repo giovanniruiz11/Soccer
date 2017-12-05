12aede396414e96009a24f44fb6642d8b1cfbb11
giovanniruiz11 <gio310ruiz@gmail.com>
2017-12-05 13:14:23 -0800

    Create goal.js

---
+++ goal.js
@@ -0,0 +1,32 @@
+
+function playCheer(e){
+	document.getElementById("crowd-noise").play();
+}
+
+function playCheer1(e){
+	document.getElementById("crowd-noise").play();
+}
+
+function playCheer2(e){
+	document.getElementById("crowd-noise").play();
+}
+
+function playCheer3(e){
+	document.getElementById("crowd-noise").play();
+}
+
+function playCheer4(e){
+	document.getElementById("crowd-noise").play();
+}
+
+function playCheer5(e){
+	document.getElementById("crowd-noise").play();
+}
+
+function playCheer6(e){
+	document.getElementById("crowd-noise").play();
+}
+
+function playCheer7(e){
+	document.getElementById("crowd-noise").play();
+}
