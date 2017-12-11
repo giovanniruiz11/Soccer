6084e0a11c0b13871ce9a3ede6ded3939fb83b6c
giovanniruiz11 <gio310ruiz@gmail.com>
2017-12-11 13:46:47 -0800

    Create goal.js

---
+++ Phase6/goal.js
@@ -0,0 +1,43 @@
+
+//the first line of each of these functions are grabing an element by Id name, "crowd-noise" and once it identifies it, it plays the sound.//-->
+//the second line of code in each function is grabing the id by name and a toogling the class "none" to the class identified.//-->
+
+function playCheer(e){
+	document.getElementById("crowd-noise").play();
+	document.getElementById("ronaldo").classList.toggle("none");
+}
+
+function playCheer1(e){
+	document.getElementById("crowd-noise").play();
+	document.getElementById("messi").classList.toggle("none");
+}
+
+function playCheer2(e){
+	document.getElementById("crowd-noise").play();
+	document.getElementById("zlatan").classList.toggle("none");
+}
+
+function playCheer3(e){
+	document.getElementById("crowd-noise").play();
+	document.getElementById("pogba").classList.toggle("none");
+}
+
+function playCheer4(e){
+	document.getElementById("crowd-noise").play();
+	document.getElementById("dybala").classList.toggle("none");
+}
+
+function playCheer5(e){
+	document.getElementById("crowd-noise").play();
+	document.getElementById("hazard").classList.toggle("none");
+}
+
+function playCheer6(e){
+	document.getElementById("crowd-noise").play();
+	document.getElementById("reus").classList.toggle("none");
+}
+
+function playCheer7(e){
+	document.getElementById("crowd-noise").play();
+	document.getElementById("debruyne").classList.toggle("none");
+}
